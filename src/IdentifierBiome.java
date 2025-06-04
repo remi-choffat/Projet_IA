7f2a0d1f6e19eededc4b08bcc5b0ec6e00ea2826
vmaxime <mister.max0303@gmail.com>
2025-06-04 10:18:41 +0200

    ajout main de test

--- src/IdentifierBiome.java
+++ src/IdentifierBiome.java
@@ -3,9 +3,12 @@ import java.awt.Image;
 import java.awt.Point;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.imageio.ImageIO;
 
 /**
  * IdentifierBiome
@@ -15,7 +18,7 @@ public class IdentifierBiome {
 
 
 
-	public BufferedImage identifier(BufferedImage img,  Color biome, Palette palette, AlgoClustering algo, int ncluster, Color[] couleurs_clusters ) {
+	public static BufferedImage identifier(BufferedImage img,  Color biome, Palette palette, AlgoClustering algo, int ncluster, Color[] couleurs_clusters ) {
 
 		
 		ArrayList<Point> points = new ArrayList<>();
@@ -30,12 +33,39 @@ public class IdentifierBiome {
 
 		int[] clusters = algo.cluster(((Point[])points.toArray()), ncluster);
 
-		BufferedImage nimg = AffichageBiome.eclaircirImage(img, 75);
+		BufferedImage nimg = AffichageBiome.eclaircirImage(img, 0.75);
 	
 		for (int i = 0; i < clusters.length; i++) {
 			var p = points.get(i);
 			img.setRGB(p.x, p.y, couleurs_clusters[clusters[i]].getRGB());	
 		}
 		return nimg;
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		BufferedImage img = ImageIO.read(new File(args[0]));
+
+		BufferedImage nimg = identifier(img, Palette.PRAIRIE, new Palette(NormeCouleur.CIELAB), new AlgoClustering() {
+			@Override
+			public int[] cluster(Point[] points, int ncluster) {
+				int[] r = new int[points.length];
+
+				
+				int midx = img.getWidth()/2;
+				int midy = img.getHeight()/2;
+
+
+				for (int i = 0; i < points.length; i++) {
+					Point p = points[i];
+
+					r[i] = p.x<midx?p.y<midy?0:1:p.y<midy?2:3;
+					
+				}
+				return r;
+			}
+		}, 4, new Color[]{
+			Color.red, Color.blue, Color.gray, Color.magenta
+		});
 	}
 }
