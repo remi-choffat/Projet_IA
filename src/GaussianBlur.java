53aa31fa1e7d0d5c19c868331821d6329a2b7736
Cesareuh <mathieu.graff498@gmail.com>
2025-06-04 10:26:15 +0200

    le flou gaussien mais ça marche

--- src/GaussianBlur.java
+++ src/GaussianBlur.java
@@ -5,28 +5,29 @@ import java.awt.image.BufferedImage;
 
 public class GaussianBlur implements Blur{
 
+	// Matrice des filtres du flou gaussien
 	public static final int SIMPLE = 0;
 	public static final int MOYEN = 1;
 	public static final int LARGE = 2;
 
 	private int[][][] filtres_gauss = {
 		{
-			{1,	2, 1},
-			{2, 4, 2},
-			{1, 2, 1},
+			{1,	2,	1},
+			{2,	4,	2},
+			{1,	2,	1},
 		},
 		{
 			{1,	4,	7,	4,	1},
-			{4,	16, 26, 16, 4},
-			{7, 26, 41, 26, 7},
-			{4,	16, 26, 16, 4},
+			{4,	16,	26,	16,	4},
+			{7,	26,	41,	26,	7},
+			{4,	16,	26,	16,	4},
 			{1,	4,	7,	4,	1},
 		},
 		{
 			{0,	0,	1,	2,	1,	0,	0},
 			{0,	3,	13,	22,	13,	3,	0},
 			{1,	13,	59,	97,	59,	13,	1},
-			{2,	22,	97,	159,97,	22,	2},
+			{2,	22,	97,	159,	97,	22,	2},
 			{1,	13,	59,	97,	59,	13,	1},
 			{0,	3,	13,	22,	13,	3,	0},
 			{0,	0,	1,	2,	1,	0,	0},
@@ -34,48 +35,50 @@ public class GaussianBlur implements Blur{
 	};
 	
 	public BufferedImage blur(BufferedImage img, int size){
-		int divide;
-		switch (size) {
-			case SIMPLE:
-				divide = 16;
-				break;
 
-			case MOYEN:
-				divide = 273;
-				break;
-
-			case LARGE:
-				divide = 1003;
-				break;
-			default:
-				divide = 16;
-				break;
-		}
+		// Sélection du filtre en fonction de la taille choisie
 		int [][] filtre = filtres_gauss[size];
+
+		// On choisi par combien sera divisé la moyenne
+		int divide = 0;
+		for (int yf = 0; yf < filtre.length; yf++) {
+			for (int xf = 0; xf < filtre.length; xf++) {
+				divide += filtre[yf][xf];
+			}
+		}
+
+		// Calcul du nombre de lignes et de colonnes de la nouvelle image
 		int new_width = img.getWidth()/filtre[0].length;
 		int new_height = img.getHeight()/filtre.length;
+
+		// Création de la nouvelle image
 		BufferedImage new_img = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
 
-		for(int y = 0; y < new_height; y++){
-			for(int x = 0; x < new_width; x++){
+		// On parcourt chaque pixel de la nouvelle image
+		for(int y = 0; y < img.getHeight(); y++){
+			for(int x = 0; x < img.getWidth(); x++){
+				// System.out.println("x : " + x + " y : " + y);
+				int d = divide;
+				// Pour chaque pixel correspondant à l'ancienne image, on calcule la moyenne du nouvel pixel
 				int[] somme = {0, 0, 0};
+				int middle = filtre.length/2;
 				for (int yf = 0; yf < filtre.length; yf++) {
 					for (int xf = 0; xf < filtre.length; xf++) {
-						int[] img_coord = {x*filtre[yf].length+xf, y*filtre.length+yf};
-						Color c = new Color(img.getRGB(img_coord[0], img_coord[1]));
-						somme[0] += c.getRed() * filtre[yf][xf];
-						somme[1] += c.getGreen() * filtre[yf][xf];
-						somme[2] += c.getBlue() * filtre[yf][xf];
+						// System.out.println("ok");
+						if(y + yf - middle >= 0 && x + xf - middle >= 0 && y + yf < img.getHeight() && x + xf < img.getWidth()){
+							int[] coord = {x+xf - middle, y+yf - middle};
+							Color c = new Color(img.getRGB(coord[0], coord[1]));
+							somme[0] += c.getRed() * filtre[yf][xf];
+							somme[1] += c.getGreen() * filtre[yf][xf];
+							somme[2] += c.getBlue() * filtre[yf][xf];
+						}else{
+							d -= filtre[yf][xf];
+						}
 					}
 				}
 				int[] moy = {somme[0]/divide, somme[1]/divide, somme[2]/divide};
 				Color rgb = new Color(moy[0], moy[1], moy[2]);
-				for (int yf = 0; yf < filtre.length; yf++) {
-					for (int xf = 0; xf < filtre.length; xf++) {
-						int[] img_coord = {x*filtre[yf].length+xf, y*filtre.length+yf};
-						new_img.setRGB(img_coord[0], img_coord[1], rgb.getRGB());
-					}
-				}
+				new_img.setRGB(x, y, rgb.getRGB());
 			}
 		}
 		return new_img;
