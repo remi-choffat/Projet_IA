8766ae898d12cecef7caadc1a453840d78d65a14
Rémi Choffat <remi.choffat1@etu.univ-lorraine.fr>
2025-06-04 08:56:25 +0200

    Ajout de la palette pour l'étiquetage des biomes

--- src/NormeCouleur.java
+++ src/NormeCouleur.java
@@ -1,69 +1,85 @@
 import java.awt.Color;
 
 /**
- * NormeCouleur
+ * Interface pour définir une norme de distance entre deux couleurs.
  */
 public interface NormeCouleur {
 
-	double distanceCouleur(Color c1, Color c2);
+    /**
+     * Calcule la distance entre deux couleurs.
+     *
+     * @param c1 la première couleur
+     * @param c2 la seconde couleur
+     * @return la distance entre les deux couleurs
+     */
+    double distanceCouleur(Color c1, Color c2);
 
-	
+    private static double r(Color c1, Color c2) {
+        return 0.5 * (c1.getRed() + c2.getRed());
+    }
 
-	private static double r(Color c1, Color c2) {
-		return 0.5 * (c1.getRed() + c2.getRed());
-	}
-	private static double dr(Color c1, Color c2) {
-		return c1.getRed() - c2.getRed();
-	}
-	private static double dg(Color c1, Color c2) {
-		return c1.getGreen() - c2.getGreen();
-	}
-	private static double db(Color c1, Color c2) {
-		return c1.getBlue() - c2.getBlue();
-	}
+    private static double dr(Color c1, Color c2) {
+        return c1.getRed() - c2.getRed();
+    }
 
+    private static double dg(Color c1, Color c2) {
+        return c1.getGreen() - c2.getGreen();
+    }
 
-
-	public final NormeCouleur REDMEANS = (Color c1, Color c2) ->  {
-			return Math.sqrt(
-				(2 + r(c1, c2)/256) * Math.pow(dr(c1, c2), 2) + 4 * Math.pow(dg(c1, c2), 2) + (
-					2 + (255 - r(c1, c2))/256 
-					) * Math.pow(db(c1, c2), 2)
-				);
-	};
-
-	public final NormeCouleur BASE =  (Color c1, Color c2) ->  {
-			return (Math.pow(dr(c1, c2), 2) + 
-					Math.pow(dg(c1, c2), 2) + 
-					Math.pow(db(c1, c2), 2)  
-					);	
-		};
+    private static double db(Color c1, Color c2) {
+        return c1.getBlue() - c2.getBlue();
+    }
 
 
-	private static double c(int[] c1) {
-		return Math.sqrt(Math.pow(c1[1], 2) + Math.pow(c1[2], 2));
-	}
+    /**
+     * Norme de distance des couleurs "REDMEANS" qui est une mesure de la différence
+     */
+    public final NormeCouleur REDMEANS = (Color c1, Color c2) -> {
+        return Math.sqrt(
+                (2 + r(c1, c2) / 256) * Math.pow(dr(c1, c2), 2) + 4 * Math.pow(dg(c1, c2), 2) + (
+                        2 + (255 - r(c1, c2)) / 256
+                ) * Math.pow(db(c1, c2), 2)
+        );
+    };
 
 
-	public final NormeCouleur CIELAB = (Color c1, Color c2) -> {
-		int[] lab1 = Utils.rgb2lab(c1);
-		int[] lab2 = Utils.rgb2lab(c2);
-
-		return Math.sqrt(
-					Math.pow(((lab1[0] - lab2[0])/1), 2) + 
-					Math.pow((c(lab1) - c(lab2))/ (1 + 0.045 * c(lab1)), 2) + 
-					Math.pow(
-						Math.sqrt(
-							Math.pow(lab1[1] - lab2[1], 2) + 
-							Math.pow(lab1[2] - lab2[2], 2) + 
-							Math.pow(c(lab1) - c(lab2), 2)
-							)/ (1 +0.015 * c(lab1)),
-						2
-						)
-				);
-	};
+    /**
+     * Norme de distance des couleurs "BASE" qui est une mesure de la différence
+     */
+    public final NormeCouleur BASE = (Color c1, Color c2) -> {
+        return (Math.pow(dr(c1, c2), 2) +
+                Math.pow(dg(c1, c2), 2) +
+                Math.pow(db(c1, c2), 2)
+        );
+    };
 
 
+    private static double c(int[] c1) {
+        return Math.sqrt(Math.pow(c1[1], 2) + Math.pow(c1[2], 2));
+    }
 
-	
+
+    /**
+     * Norme de distance des couleurs "CIELAB" qui est une mesure de la différence
+     * basée sur l'espace colorimétrique CIELAB.
+     */
+    public final NormeCouleur CIELAB = (Color c1, Color c2) -> {
+        int[] lab1 = Utils.rgb2lab(c1);
+        int[] lab2 = Utils.rgb2lab(c2);
+
+        return Math.sqrt(
+                Math.pow(((double) (lab1[0] - lab2[0])), 2) +
+                        Math.pow((c(lab1) - c(lab2)) / (1 + 0.045 * c(lab1)), 2) +
+                        Math.pow(
+                                Math.sqrt(
+                                        Math.pow(lab1[1] - lab2[1], 2) +
+                                                Math.pow(lab1[2] - lab2[2], 2) +
+                                                Math.pow(c(lab1) - c(lab2), 2)
+                                ) / (1 + 0.015 * c(lab1)),
+                                2
+                        )
+        );
+    };
+
+
 }
---
+++ src/Palette.java
@@ -0,0 +1,80 @@
+import java.awt.*;
+
+/**
+ * Représente une palette de couleurs prédéfinies pour des environnements naturels.
+ */
+public class Palette {
+
+    public static final Color TUNDRA = new Color(71, 70, 61);
+    public static final Color TAIGA = new Color(43, 50, 35);
+    public static final Color FORET_TEMPEREE = new Color(59, 66, 43);
+    public static final Color FORET_TROPICALE = new Color(46, 64, 34);
+    public static final Color SAVANE = new Color(84, 106, 70);
+    public static final Color PRAIRIE = new Color(104, 95, 82);
+    public static final Color DESERT = new Color(152, 140, 120);
+    public static final Color GLACIER = new Color(200, 200, 200);
+    public static final Color EAU_PEU_PROFONDE = new Color(49, 83, 100);
+    public static final Color EAU_PROFONDE = new Color(12, 31, 47);
+
+    /**
+     * Tableau de couleurs prédéfinies
+     */
+    private final Color[] couleurs = new Color[]{
+            TUNDRA, // Toundra
+            TAIGA, // Taïga
+            FORET_TEMPEREE, // Forêt tempérée
+            FORET_TROPICALE, // Forêt tropicale
+            SAVANE, // Savane
+            PRAIRIE, // Prairie
+            DESERT, // Désert
+            GLACIER, // Glacier
+            EAU_PEU_PROFONDE, // Eau peu profonde
+            EAU_PROFONDE  // Eau profonde
+    };
+
+
+    /**
+     * Norme de distance des couleurs utilisée pour comparer les couleurs
+     */
+    private final NormeCouleur distanceCouleurs;
+
+
+    /**
+     * Constructeur de la classe Palette
+     *
+     * @param distanceCouleurs la norme de distance des couleurs
+     */
+    public Palette(NormeCouleur distanceCouleurs) {
+        this.distanceCouleurs = distanceCouleurs;
+    }
+
+
+    /**
+     * Renvoie le tableau de couleurs prédéfinies
+     *
+     * @return le tableau de couleurs
+     */
+    public Color[] getCouleurs() {
+        return couleurs;
+    }
+
+
+    /**
+     * Récupère la couleur la plus proche d'une couleur donnée dans le tableau des couleurs prédéfinies
+     *
+     * @param c la couleur à comparer
+     * @return la couleur la plus proche dans le tableau couleurs
+     */
+    public Color getPlusProche(Color c) {
+        int closestColorIndex = 0;
+        double minDistance = Double.MAX_VALUE;
+        for (int k = 0; k < couleurs.length; k++) {
+            double distance = distanceCouleurs.distanceCouleur(c, couleurs[k]);
+            if (distance < minDistance) {
+                minDistance = distance;
+                closestColorIndex = k;
+            }
+        }
+        return couleurs[closestColorIndex];
+    }
+}
